5718ac2bf603e2d140262fa069490bec3d636792
Domen Kogler <domen.kogler@gmail.com>
2015-07-26 10:59:15 +0200

    start

---
+++ Assembler/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Windows;
+
+[assembly: AssemblyTitle("SerialCOM")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("SerialCOM")]
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+[assembly: AssemblyCompany("Kogler")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
+
+[assembly: AssemblyVersion("0.3.*")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
